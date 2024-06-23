d301ef9c6caca8d8e71ced8c5bdd7d095222afac
mirrorgo <cloudmirrorgo@163.com>
2024-06-23 10:10:53 +1000

    feat: can play audio

---
+++ components/AudioPlayer.tsx
@@ -0,0 +1,20 @@
+function AudioPlayer() {
+  return (
+    <div>
+      wowo
+      <div>
+        <figure>
+          <figcaption>Listen to the T-Rex:</figcaption>
+          {/* <audio controls src="/media/cc0-audio/t-rex-roar.mp3"></audio> */}
+          <audio
+            controls
+            src="http://server.unimelb.top/public/music/myaudio.m4a"
+          ></audio>
+          {/* <a href="/media/cc0-audio/t-rex-roar.mp3"> Download audio </a> */}
+        </figure>
+      </div>
+    </div>
+  );
+}
+
+export default AudioPlayer;
