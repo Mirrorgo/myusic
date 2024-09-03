7d120daffd4c6fc187905768e471ae78fa6d11c2
mirrorgo <cloudmirrorgo@163.com>
2024-09-03 10:18:19 +1000

    feat: remove middleware

--- middleware.ts
+++ middleware.ts
@@ -10,23 +10,21 @@ export function middleware(request: {
 }) {
   // 解析 Cookie
   // cookie这部分只有next使用，后端是不用的
-  const cookieHeader = request.headers.get("cookie");
-  const cookies = cookieHeader ? parse(cookieHeader) : {};
-  const token = cookies.token;
-
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  try {
-    // TODO： 通过环境变量进行jwt next中间件的校验
-    // jwt.verify(token, SECRET_KEY);
-    return NextResponse.next();
-  } catch (error) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  // const cookieHeader = request.headers.get("cookie");
+  // const cookies = cookieHeader ? parse(cookieHeader) : {};
+  // const token = cookies.token;
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
+  // try {
+  //   // TODO： 通过环境变量进行jwt next中间件的校验
+  //   // jwt.verify(token, SECRET_KEY);
+  //   return NextResponse.next();
+  // } catch (error) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
 }
 
-export const config = {
-  matcher: ["/", "/home"], // 保护的路径
-};
+// export const config = {
+//   matcher: ["/", "/home"], // 保护的路径
+// };
